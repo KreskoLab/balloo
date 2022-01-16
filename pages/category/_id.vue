2481042bbd3a085206c6aa7b24204bf1e99d0008
andrii <redeezol@gmailc.om>
2022-01-16 22:07:12 +0200

    categories page updates

--- pages/category/_id.vue
+++ pages/category/_id.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
         
-        <div class="flex flex-row justify-between">
-            <h2 class="text-2xl font-medium">{{$route.params.id}}</h2>
+      <div class="flex flex-row justify-between">
+          <h2 class="text-2xl font-medium">{{category.name}}</h2>
 
-            <div class="">
-                sadsad
-            </div>
-        </div>
+          <div class="font-medium">
+              дешевші
+          </div>
+      </div>
+
+      <Card
+        :name="products[0].name"
+      />
         
 
     </div>
@@ -15,16 +19,27 @@
 
 <script>
 export default {
-    async fetch() {
-        await this.$axios.$get(`api/category/${this.$route.params.id}`)
-        .then((res) => {
-            this.category = res
-        })
-    },
-    data() {
-        return {
-            category: {}
-        }
+  async fetch() {
+    await this.$axios.$get(`api/products?${this.subcategories_query}`)
+    .then((res) => {
+      this.products = res.map(product => ({ ...product, name: product.name.find(item => item.lang === 'ua').value}))
+    })
+  },
+  data() {
+    return {
+      products: []
     }
+  },
+  computed: {
+    category() {
+      return this.$store.getters['categories/getCategoryBySlug'](this.$route.params.id)
+    },
+    subcategories_query() {
+      let query = ''
+      this.$store.getters['categories/getSubcategoriesBySlug'](this.$route.params.id).map(({_id}) => _id)
+          .forEach(id => query += `subcategories=${id}&`)
+      return query
+    }
+  }
 }
 </script>
