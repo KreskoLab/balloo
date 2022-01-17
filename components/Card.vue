88483047afc6b32d98b9851af59020426436076f
andrii <redeezol@gmailc.om>
2022-01-17 15:55:12 +0200

    component props

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col space-y-2 transition-all ease-in-out duration-300 hover:(cursor-pointer p-2)">
-    <img class="rounded-sm" src="https://www.gingerray.co.uk/static/media/catalog/product/a/s/MIX-233_1.jpg" alt="image" />
-    <h1 class="font-medium text-xl text-dark-800">Фольгированые шарики</h1>
+    <img class="rounded-sm" :src="image" :alt="name" />
+    <h1 class="font-medium text-xl text-dark-800">{{name}}</h1>
   </div>
 </template>
 
