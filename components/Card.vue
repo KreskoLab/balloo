cc0de6b1b3ded64ff5a919809e402978b02c0694
andrii <redeezol@gmailc.om>
2022-01-17 15:52:22 +0200

    index page rework with new Card component

--- components/Card.vue
+++ components/Card.vue
@@ -1,41 +1,26 @@
 <template>
-    <div class="flex flex-col animate-slower animate-fadeIn">
-
-        <div class="w-full border-2 relative border-dark-500 bg-blue-gray-50 hover:(bg-orange-100 transition duration-200)">
-
-            <svg class="hover:(stroke-amber-500 cursor-pointer transition duration-150) z-50 absolute top-2 right-0 mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-            </svg>
-
-            <nuxt-img 
-                class="py-8 h-full mx-auto hover:(animate-animated animate-infinite animate-headShake cursor-pointer)" 
-                width="320" 
-                height="320" 
-                :src="`./${image}.png`"
-                format="webp"
-                alt="test" 
-            />
-
-        </div>
-
-        <div class="py-2">
-            
-            <div class="flex flex-row justify-between items-center text-base sm:text-lg font-medium">
-
-                <h1>шарик синий</h1>
-                <span>90 ₴</span>
-
-            </div>
-
-        </div>
-
-    </div>
+  <div class="flex flex-col space-y-2 transition-all ease-in-out duration-300 hover:(cursor-pointer p-2)">
+    <img class="rounded-sm" src="https://www.gingerray.co.uk/static/media/catalog/product/a/s/MIX-233_1.jpg" alt="image" />
+    <h1 class="font-medium text-xl text-dark-800">Фольгированые шарики</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        image: String
+  name: "Card",
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    image: {
+      type: String,
+      default: ''
     }
+  }
 }
 </script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="w-full">
 
-    <div class="grid grid-cols-2 gap-x-6 gap-y-3 sm:(grid-cols-3 gap-x-6 gap-y-3)">
+    <div class="grid grid-cols-2 gap-x-3 gap-y-6 sm:(grid-cols-4 gap-x-12 gap-y-6)">
 
-      <Card :image="'test'" />
-
-      <Card :image="'3'" />
-
-      <Card :image="'3'" />
-
-      <Card :image="'test2'" />
-
-      <Card :image="'3'" />
-
-      <Card :image="'test'" />
-
+      <div v-for="card in cards" :key="card.name">
+        <Card
+          :name="card.name"
+          :image="card.image"
+        />
+      </div>
 
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-  created() {
-    //this.$store.dispatch('user/getUser')
+  data() {
+    return {
+      cards: [
+        {
+          name: 'Фольгированые шарики',
+          image: 'https://www.gingerray.co.uk/static/media/catalog/product/a/s/MIX-233_1.jpg'
+        },
+        {
+          name: 'Латексные шарики',
+          image: 'https://www.gingerray.co.uk/static/media/catalog/product/a/s/MIX-470_1.jpg'
+        }
+      ]
+    }
   }
 }
 </script>
