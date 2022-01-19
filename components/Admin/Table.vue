33c6d13636caa69a9b81e0e04aec6e41efa4d636
KreskoLab <redeezol@gmail.com>
2022-01-19 22:59:25 +0200

    updated table headers font weight

--- components/Admin/Table.vue
+++ components/Admin/Table.vue
@@ -6,7 +6,7 @@
                 <div 
                     v-for="(header, i) in headers" 
                     :key="i" 
-                    class="table-cell font-medium"
+                    class="table-cell"
                 >
                     {{header}}
                 </div>
@@ -22,21 +22,34 @@
                 <div 
                     v-for="value in item"  
                     :key="value"
-                    class="table-cell py-2"
+                    class="table-cell font-medium py-2"
                 >
                     {{ value }}
                 </div>
 
-                <div class="table-cell py-2 mx-auto">
-                    <svg 
-                        class="h-6 w-6 cursor-pointer hover:(stroke-red-500)" 
-                        fill="none" 
-                        viewBox="0 0 24 24" 
+                <div class="table-cell mx-auto py-2">
+                  <div class="flex space-x-4">
+                    <svg
+                        class="h-6 w-6 hover:(stroke-red-500 cursor-pointer)"
+                        fill="none"
+                        viewBox="0 0 24 24"
                         stroke="currentColor"
                         @click="showModal(); target = item"
                     >
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
+
+                    <svg
+                        class="h-6 w-6 hover:(stroke-amber-500 cursor-pointer)"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        @click="edit(item)"
+                    >
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                    </svg>
+                  </div>
+
                 </div>
             </div>
         </div>
@@ -55,32 +68,45 @@
 <script>
 export default {
     props: {
-        data: Array,
-        headers: Array,
-        type: String
+      data: {
+        type: Array,
+        default: () => []
+      },
+      headers: {
+        type: Array,
+        default: () =>[]
+      },
+      type: String
     },
     data() {
-        return {
-            modal: false,
-            target: {}
-        }
+      return {
+          modal: false,
+          target: {}
+      }
     },
     computed: {
-        filtered_data() {
-            return this.data.map(({ _id, ...rest }) => rest)
-        }
+      filtered_data() {
+          return this.data.map(({ _id, ...rest }) => rest)
+      }
     },
     methods: {
-        showModal() {
-            this.modal = true
-        },
-        closeModal() {
-            this.modal = false
-        },
-        async removeTarget() {
-            let item_to_remove = this.data.find(item => item.name == this.target.name)
-            this.$emit('remove', item_to_remove._id)
-        }
+      showModal() {
+        this.modal = true
+      },
+      closeModal() {
+        this.modal = false
+      },
+      findID(target) {
+        return this.data.find(item => item.name === target.name)._id
+      },
+      removeTarget() {
+        let id = this.findID(this.target)
+        this.$emit('remove', id)
+      },
+      edit(item) {
+        let id = this.findID(item)
+        this.$emit('edit', id)
+      }
     }
 }
 </script>
