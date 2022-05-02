2d5b8cefc15436ce7c658558d5dbcfe9b1fd0f75
KreskoLab <redeezol@gmail.com>
2022-05-03 00:00:54 +0300

    feat: add Index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,30 @@
 <template>
-  <div class="page">
-
-    <div class="grid grid-cols-2 gap-x-6 gap-y-4 sm:(grid-cols-4 gap-x-12 gap-y-6)">
-
-      <div v-for="subcategory in subcategories" :key="subcategory.slug">
-        <Card
-          :name="subcategory.name"
-          :slug="subcategory.slug"
-          :category="subcategory.category"
-          :image="subcategory.image"
-        />
-      </div>
-
+  <div class="page pb-6">
+    <div class="grid grid-cols-2 gap-x-6 gap-y-4 lg:(grid-cols-4 gap-x-12 gap-y-6)">
+      <UiCategoryCard
+        v-for="subcategory in subcategories"
+        :key="subcategory.slug"
+        :name="subcategory.name"
+        :slug="subcategory.slug"
+        :category="subcategory.category"
+        :image="subcategory.image"
+      />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  created() {
-    this.subcategories = this.$store.getters['categories/getSubcategories']
+  computed: {
+    subcategories() {
+      return this.$store.getters['categories/getSubcategories']
+    },
   },
-  data() {
-    return {
-      subcategories: [],
-      cards: [
-        {
-          name: 'Цифры',
-          image: 'https://www.gingerray.co.uk/static/media/catalog/product/a/s/SG-102_1.jpg'
-        },
-        {
-          name: 'Звезды',
-          image: 'https://www.gingerray.co.uk/static/media/catalog/product/a/s/HE-242_1.jpg'
-        },
-        {
-          name: 'Сердечки',
-          image: 'https://www.gingerray.co.uk/static/media/catalog/product/a/s/HE-233_1.jpg'
-        },
-        {
-          name: 'Фигурки',
-          image: 'https://www.gingerray.co.uk/static/media/catalog/product/a/s/MW-119_1.jpg'
-        },
-        {
-          name: 'Обычные',
-          image: 'https://www.gingerray.co.uk/static/media/catalog/product/a/s/MIX-470_1.jpg'
-        },
-        {
-          name: 'С рисунком',
-          image: 'https://www.gingerray.co.uk/static/media/catalog/product/a/s/DA-105_1.jpg'
-        }
-      ]
-    }
-  }
+
+  watch: {
+    '$i18n.locale': async function () {
+      await this.$store.dispatch('categories/getCategories')
+    },
+  },
 }
 </script>
