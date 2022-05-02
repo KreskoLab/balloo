5d58b88c5545c043a6487a41b70551640ff2ad27
KreskoLab <redeezol@gmail.com>
2022-05-02 23:54:45 +0300

    feat: add Favorites store

--- store/favorites.js
+++ store/favorites.js
@@ -1,16 +1,14 @@
 export const state = () => ({
-    list: []
+  list: [],
 })
 
 export const mutations = {
-
-    setItem(state, id) {
-        if (state.list.includes(id)) {
-            let index = state.list.indexOf(id)
-            state.list.splice(index, 1)
-        } else {
-            state.list.push(id)
-        }
+  setItem(state, payload) {
+    if (state.list.some((item) => item.product === payload.product)) {
+      const index = state.list.findIndex((item) => item.product === payload.product)
+      state.list.splice(index, 1)
+    } else {
+      state.list.push(payload)
     }
-
+  },
 }
