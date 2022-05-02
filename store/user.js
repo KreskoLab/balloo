3e3eb718216d1c3c8233a3c9db8a0fadfd124cec
KreskoLab <redeezol@gmail.com>
2022-05-02 23:55:27 +0300

    feat: add User store

--- store/user.js
+++ store/user.js
@@ -1,40 +1,37 @@
 export const state = () => ({
   user: {},
-  loggedIn: false
+  loggedIn: false,
 })
 
 export const mutations = {
-
   setUser(state, val) {
     state.user = val
   },
 
   setLoggedIn(state, val) {
     state.loggedIn = val
-  }
+  },
 }
 
 export const actions = {
-
   async getUser({ commit }) {
+    await this.$axios
+      .$get('api/me')
+      .then((res) => {
+        commit('setUser', res)
+        commit('setLoggedIn', true)
+      })
+      .catch((err) => {
+        console.log(err)
+        commit('setLoggedIn', false)
+        commit('setUser', {})
+      })
+  },
 
-    await this.$axios.$get('api/me')
-    .then((res) => {
-      commit('setUser', res.user)
-      commit('setLoggedIn', true)
-    })
-    .catch((err) => {
+  async logout({ commit }) {
+    await this.$axios.$get('api/logout').then(() => {
       commit('setLoggedIn', false)
       commit('setUser', {})
     })
   },
-
-  async logout({ commit }) {
-
-    await this.$axios.$get('api/logout')
-    .then(() => {
-      commit('setLoggedIn', false)
-      commit('setUser', {})
-    })
-  }
 }
