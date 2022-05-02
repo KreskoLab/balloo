3b3af9d98728ab5db4fa4ec3a340b646a25b9737
KreskoLab <redeezol@gmail.com>
2022-05-02 23:53:06 +0300

    feat: add Default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,36 @@
 <template>
-    <div class="w-full bg-light-100">
-
-      <div class="w-full fixed top-0 z-50 bg-light-100">
-        <Header/>
-        <Navbar/>
-      </div>
-
-      <div class="flex container mx-auto !max-w-screen-2xl h-screen pt-20 lg:(pt-30)">
-
-        <div
-            class="hidden sm:(block w-1/4 pt-6 px-6 overflow-y-auto overflow-x-hidden)"
-            v-if="$route.name !== 'index' && $route.name !== 'favorites'"
-        >
-          <Sidebar />
-        </div>
-
-        <div class="relative w-full h-full overflow-hidden">
-          <Nuxt />
-          <Cart />
-          <FiltersSheet />
-        </div>
-      </div>
-
-      <transition
-          enter-active-class="animate-animated animate-faster animate-slideInLeft"
-          leave-active-class="animate-animated animate-faster animate-slideOutLeft"
-          mode="out-in"
-          appear
-      >
-        <div class="absolute top-0 w-full h-full sm:(hidden)" v-if="showHamburgerMenu">
-          <HamburgerMenu />
-        </div>
-      </transition>
-
+  <div class="w-full bg-light-100">
+    <div class="w-full fixed top-0 z-50 bg-light-100">
+      <TheFooter />
+      <NavBar />
     </div>
+
+    <div class="relative flex w-full h-screen overflow-y-auto overflow-x-hidden pt-20 lg:(pt-30)">
+      <SideBar v-if="$route.name !== 'index' && $route.name !== 'favorites'" />
+
+      <main class="w-full">
+        <Nuxt />
+      </main>
+
+      <TheCart />
+
+      <SideBarBottomSheet />
+    </div>
+
+    <transition
+      enter-active-class="animate-animated animate-faster animate-slideInLeft"
+      leave-active-class="animate-animated animate-faster animate-slideOutLeft"
+      mode="out-in"
+      appear
+    >
+      <div
+        v-if="showHamburgerMenu"
+        class="absolute top-0 w-full h-full lg:(hidden)"
+      >
+        <TheHamburgerMenu />
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -41,18 +38,18 @@ export default {
   computed: {
     showHamburgerMenu() {
       return this.$store.state.showHamburgerMenu
-    }
-  }
+    },
+  },
 }
 </script>
 
 <style>
-  .page-enter-active,
-  .page-leave-active {
-    transition: opacity 0.3s;
-  }
-  .page-enter,
-  .page-leave-to {
-    opacity: 0;
-  }
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.3s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
 </style>
