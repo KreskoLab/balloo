91a25692e8e535589b727acd9aa390e4004688c7
KreskoLab <redeezol@gmail.com>
2022-05-03 00:01:04 +0300

    feat: add Favorites page

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,96 +1,78 @@
 <template>
   <div class="page">
-    <h1 class="text-2xl font-semibold">{{title}}</h1>
-    <h2 class="text-dark-200 font-medium mt-4">{{subtitle}}</h2>
+    <h1 class="text-2xl font-semibold">
+      <template v-if="favorites.length">
+        {{ $t('favorites.title') }}
+      </template>
+
+      <template v-else>
+        {{ $t('favorites.titleEmpty') }}
+      </template>
+    </h1>
+    <h2 class="text-dark-200 font-medium mt-4">
+      {{ $t('favorites.subtitle') }}
+    </h2>
 
     <div v-if="!$fetchState.pending">
-      <div class="flex flex-row gap-x-12 my-6" v-if="products.length > 0">
-        <Product
-            v-for="product in products"
-            :key="product._id"
-            :name="product.name"
-            :slug="product.slug"
-            :price="product.price"
-            :image="product.image"
+      <div
+        v-if="products.length > 0"
+        class="grid grid-cols-2 gap-x-4 gap-y-4 my-8 sm:(grid-cols-4 gap-x-8 gap-y-4)"
+      >
+        <ProductCard
+          v-for="product in products"
+          :key="product._id"
+          :name="product.name"
+          :slug="product.slug"
+          :price="product.price"
+          :image="product.image"
+          :category="product.category"
         />
       </div>
 
-      <div class="flex justify-center items-center my-16" v-else>
+      <div
+        v-else
+        class="flex justify-center items-center my-16"
+      >
         <nuxt-link to="/">
-          <button class="btn_shop">
-            Продовжити покупки
-          </button>
+          <UiButton color="bg-red-200"> {{ $t('favorites.shop') }}</UiButton>
         </nuxt-link>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-  name: "favorites",
+  data() {
+    return {
+      products: [],
+    }
+  },
+
   async fetch() {
     if (this.favorites.length > 0) {
       let query = ''
-      this.favorites.forEach(item => query += `slugs=${item}&`)
-
-      await this.$axios.$get(`api/products?${query}`)
-      .then((res) => {
-        this.products = res.map(({ name, subcategories, ... rest }) => ({ name: name.value, ...rest }))
+      this.favorites.forEach((item) => (query += `slugs=${item.product}&`))
+      await this.$axios.$get(`api/products?${query}`).then((res) => {
+        this.products = res.map(({ name, subcategories, ...rest }, index) => ({
+          name: name.value,
+          category: this.favorites[index].category,
+          ...rest,
+        }))
       })
     }
   },
-  data() {
-    return {
-      products: []
-    }
-  },
+
   computed: {
-    lang() {
-      return this.$store.state.language.lang
-    },
-    title() {
-      if (this.favorites.length > 0) {
-        return this.lang === 'uk' ? 'Обрані товари' : 'Избранные товары'
-      }
-      else {
-        return this.lang === 'uk' ? 'Ваш список улюблених товарів пустий' : 'Ваш список любимых товаров пустой'
-      }
-    },
-    subtitle() {
-      return this.lang === 'uk' ? 'Збережи товари на потім, щоб не забути про них': 'Сохрани товары на потом, чтобы не забыть о них'
-    },
     favorites() {
       return this.$store.state.favorites.list
-    }
+    },
   },
+
   watch: {
     favorites() {
-      this.products = this.products.filter(item => this.favorites.includes(item.slug))
-    }
-  }
+      this.products = this.products.filter((item) => this.favorites.some((favorite) => item.slug === favorite.product))
+    },
+  },
 }
 </script>
-
-<style scoped>
-  .btn_shop {
-    @apply
-    appearance-none
-    relative
-    bg-green-200 border-dark-600 border-2
-    px-6 py-2
-    font-medium text-lg
-    transition-all transform duration-0
-    hover:(cursor-pointer translate-y-3px)
-    after:(content-DEFAULT absolute bottom-0 -right-2px -left-2px border-transparent border-6 transform translate-y-12px)
-  }
-
-  .btn_shop:after {
-    border-top-color: #1c1c1e;
-  }
-
-  .btn_shop:hover:after {
-    @apply translate-y-8px border-4 right-0 left-0
-  }
-</style>
