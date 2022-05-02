a47bc4735943e5527da27427d4700ceba3c123b7
KreskoLab <redeezol@gmail.com>
2022-05-02 23:54:57 +0300

    feat: add Filters store

--- store/filters.js
+++ store/filters.js
@@ -1,47 +1,47 @@
 export const state = () => ({
-    list: [],
-    listCopy: [],
-    active: [],
-    query: ""
+  list: [],
+  listCopy: [],
+  active: [],
+  query: '',
 })
 
 export const mutations = {
+  setFilters(state, newFilters) {
+    state.list = newFilters
+  },
 
-    setFilters(state, newFilters) {
-      state.list = newFilters
-    },
+  setFiltersCopy(state, copy) {
+    state.listCopy = copy
+  },
 
-    setFiltersCopy(state, copy) {
-        state.listCopy = copy
-    },
+  setActiveFilter(state, filters) {
+    state.active = filters
+  },
 
-    setActiveFilter(state, filters) {
-      state.active = filters
-    },
+  setAvailableFilters(state, newFilters) {
+    const localCopy = JSON.parse(JSON.stringify(state.listCopy))
 
-    setAvailableFilters(state, newFilters) {
-        let localCopy = JSON.parse(JSON.stringify(state.listCopy))
+    state.list = localCopy.map((item) => {
+      item.filters = item.filters.filter((element) => {
+        return (
+          newFilters.some((filter) => filter.valueSlug === element.value) ||
+          state.active.every((active) => item.filters.some((fil) => fil.value === active.value))
+        )
+      })
+      return item
+    })
+  },
 
-        state.list = localCopy.map((item) => {
-            item.filters = item.filters.filter(element => {
-                return newFilters.some(filter => filter.value_slug === element.value)
-                    || state.active.every(active => item.filters.some(fil => fil.value === active.value))
-            })
-            return item
-        })
-    },
-
-    setQuery(state, query) {
-        state.query = query
-    }
+  setQuery(state, query) {
+    state.query = query
+  },
 }
 
 export const actions = {
-
-    async getFilters({ commit }, id) {
-        await this.$axios.$get(`api/subcategory/${id}/filters`).then((res) => {
-            commit("setFilters", res)
-            commit("setFiltersCopy", res)
-        })
-    }
+  async getFilters({ commit }, id) {
+    await this.$axios.$get(`api/subcategory/${id}/filters`).then((res) => {
+      commit('setFilters', res)
+      commit('setFiltersCopy', res)
+    })
+  },
 }
