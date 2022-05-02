c85796ac19367428d2256b0035ee9703d8c7aaa0
KreskoLab <redeezol@gmail.com>
2022-05-02 23:55:18 +0300

    feat: add store

--- store/index.js
+++ store/index.js
@@ -1,19 +1,21 @@
 export const state = () => ({
-  showHamburgerMenu: false
+  showHamburgerMenu: false,
+  showFiltersSheet: false,
 })
 
 export const mutations = {
-
   setHamburgerMenu(state) {
     state.showHamburgerMenu = !state.showHamburgerMenu
-  }
+  },
 
+  setFiltersSheet(state, val) {
+    state.showFiltersSheet = val
+  },
 }
 
 export const actions = {
-
   async nuxtServerInit({ dispatch }) {
     await dispatch('user/getUser')
     await dispatch('categories/getCategories')
-  }
+  },
 }
