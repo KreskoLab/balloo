089347088ccd88e69a3793fe3218524ce07c0afe
KreskoLab <redeezol@gmail.com>
2022-05-02 23:54:15 +0300

    feat: add Categories store

--- store/categories.js
+++ store/categories.js
@@ -1,85 +1,86 @@
 export const state = () => ({
-    list: [],
-    subcategory: {}
-}) 
+  list: [],
+  subcategory: {},
+})
 
 export const mutations = {
+  setList(state, categories) {
+    state.list = categories.map((category) => ({
+      ...category,
+      name: category.name.value,
+      subcategories: category.subcategories.map(({ products, name, ...rest }) => ({
+        ...rest,
+        name: name.value,
+        products: products.length,
+      })),
+    }))
+  },
 
-    setList(state, categories) {
-        state.list = categories.map(category => ({
-            ...category,
-            name: category.name.value,
-            subcategories: category.subcategories.map(({ products, name, ...rest }) => ({
-                ...rest,
-                name: name.value,
-                products: products.length
-            }))
-        }))
-    },
+  setSubcategory(state, slug) {
+    for (const category of state.list) {
+      const subcategory = category.subcategories.find((subcategory) => subcategory.slug === slug)
 
-    setSubcategory(state, slug) {
-        for (const category of state.list) {
-            let subcategory = category.subcategories.find(subcategory => subcategory.slug === slug)
-
-            if (subcategory) {
-                state.subcategory = subcategory
-            }
-        }
+      if (subcategory) {
+        state.subcategory = subcategory
+      }
     }
+  },
 }
 
 export const actions = {
-
-    async getCategories({ commit }) {
-        await this.$axios.$get('api/categories')
-        .then((res) => {
-            commit('setList', res)
-        })
-        .catch((err) => {
-
-        })
-    }
+  async getCategories({ commit }) {
+    await this.$axios
+      .$get('api/categories')
+      .then((res) => {
+        commit('setList', res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
 }
 
 export const getters = {
-
-    getSubcategoriesBySlug: (state) => slug => {
-        if (state.list.length > 0) {
-            return state.list.find(category => category.slug === slug).subcategories
-        } else {
-            return []
-        }
-    },
-
-    getCategoryBySlug: (state) => slug => {
-        if (state.list.length > 0) {
-            return state.list.find(category => category.slug === slug)
-        } else {
-            return {}
-        }
-    },
-
-    getSubcategory: (state) => {
-        return state.subcategory
-    },
-
-    getSubcategoryBySlug: (state) => slug => {
-        for (const category of state.list) {
-            let subcategory = category.subcategories.find(subcategory => subcategory.slug === slug)
-
-            if (subcategory) {
-                return subcategory
-            }
-        }
-    },
-
-    getSubcategories: (state) => {
-        let list_copy = JSON.parse(JSON.stringify(state.list))
-
-        list_copy.forEach((category, index, arr) => {
-            arr[index].subcategories = category.subcategories.map(subcategory => ({...subcategory, category: category.slug}))
-        })
-
-        return list_copy.map(category => category.subcategories).flat()
+  getSubcategoriesBySlug: (state) => (slug) => {
+    if (state.list.length > 0) {
+      return state.list.find((category) => category.slug === slug).subcategories
+    } else {
+      return []
     }
+  },
+
+  getCategoryBySlug: (state) => (slug) => {
+    if (state.list.length > 0) {
+      return state.list.find((category) => category.slug === slug)
+    } else {
+      return {}
+    }
+  },
+
+  getSubcategory: (state) => {
+    return state.subcategory
+  },
+
+  getSubcategoryBySlug: (state) => (slug) => {
+    for (const category of state.list) {
+      const subcategory = category.subcategories.find((subcategory) => subcategory.slug === slug)
+
+      if (subcategory) {
+        return subcategory
+      }
+    }
+  },
+
+  getSubcategories: (state) => {
+    const listCopy = JSON.parse(JSON.stringify(state.list))
+
+    listCopy.forEach((category, index, arr) => {
+      arr[index].subcategories = category.subcategories.map((subcategory) => ({
+        ...subcategory,
+        category: category.slug,
+      }))
+    })
+
+    return listCopy.map((category) => category.subcategories).flat()
+  },
 }
