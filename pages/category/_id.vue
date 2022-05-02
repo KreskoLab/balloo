03b759cf1ab6834236d78ed37e24b29bc578335e
KreskoLab <redeezol@gmail.com>
2022-05-03 00:01:46 +0300

    feat: add Category page

--- pages/category/_id.vue
+++ pages/category/_id.vue
@@ -1,63 +1,80 @@
 <template>
   <div>
-
     <div
-      class="page"
-      v-if="$route.name !== 'category-id-subcategory' && $route.name !== 'category-id-subcategory-product-page' && !$fetchState.pending"
+      v-if="show"
+      class="page lg:(ml-[320px])"
     >
       <div class="flex flex-row justify-between items-center">
         <h2 class="text-2xl font-medium">{{ category.name }}</h2>
-
-        <div class="font-medium">
-          дешевші
-        </div>
+        <div class="font-medium">дешевші</div>
       </div>
 
       <div class="grid grid-cols-2 gap-x-4 gap-y-4 my-8 sm:(grid-cols-3 gap-x-8 gap-y-4)">
         <div
-            v-for="product in products"
-            :key="product._id"
+          v-for="product in products"
+          :key="product._id"
         >
-          <Product
+          <ProductCard
             :name="product.name"
             :image="product.image"
             :slug="product.slug"
             :price="product.price"
+            :category="$route.params.id"
           />
         </div>
       </div>
     </div>
 
-    <div>
-      <nuxt-child/>
-    </div>
-
+    <nuxt-child />
   </div>
 </template>
 
 <script>
 export default {
-  async fetch() {
-    await this.$axios.$get(`api/products?${this.subcategories_query}`)
-        .then((res) => {
-          this.products = res.map(product => ({ ...product, name: product.name.value}))
-        })
-  },
   data() {
     return {
-      products: []
+      products: [],
     }
   },
+
+  async fetch() {
+    this.products = await this.$axios.$get(`api/products?${this.subcategories_query}`).then((res) => {
+      return res.map((product) => ({
+        ...product,
+        name: product.name.value,
+      }))
+    })
+  },
+
   computed: {
     category() {
       return this.$store.getters['categories/getCategoryBySlug'](this.$route.params.id)
     },
+
+    show() {
+      if (
+        this.$route.name !== 'category-id-subcategory' &&
+        this.$route.name !== 'category-id-subcategory-product-page' &&
+        !this.$fetchState.pending
+      ) {
+        return true
+      } else return false
+    },
+
     subcategories_query() {
       let query = ''
-      this.$store.getters['categories/getSubcategoriesBySlug'](this.$route.params.id).map(({_id}) => _id)
-          .forEach(id => query += `subcategories=${id}&`)
+      this.$store.getters['categories/getSubcategoriesBySlug'](this.$route.params.id)
+        .map(({ _id }) => _id)
+        .forEach((id) => (query += `subcategories=${id}&`))
       return query
-    }
-  }
+    },
+  },
+
+  watch: {
+    '$i18n.locale': async function () {
+      await this.$store.dispatch('categories/getCategories')
+      this.$fetch()
+    },
+  },
 }
 </script>
