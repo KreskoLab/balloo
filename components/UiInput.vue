266a4e1123353130a75e5bb90c2abdbdd4fdec43
KreskoLab <redeezol@gmail.com>
2022-05-10 23:17:18 +0300

    feat: add Input component

---
+++ components/UiInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <label class="block mb-2">{{ label }}</label>
+    <input
+      class="bg-light-100 border border-dark-600 rounded focus:ring-dark-600 focus:border-dark-600 outline-none p-2.5"
+      :type="type"
+      :value="value"
+      :required="required"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiInput',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+
+    value: {
+      type: String,
+      default: '',
+    },
+
+    type: {
+      type: String,
+      default: 'text',
+    },
+
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
