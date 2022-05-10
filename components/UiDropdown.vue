7791f12927fefc18b14077b3d81b882b0da51042
KreskoLab <redeezol@gmail.com>
2022-05-10 23:17:05 +0300

    feat: add Dropdown component

---
+++ components/UiDropdown.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="relative w-max">
+    <button @click="active = !active">
+      <slot name="header" />
+    </button>
+
+    <transition
+      enter-active-class="animate-animated animate-faster animate-bounceIn"
+      leave-active-class="animate-animated animate-faster animate-slideOutRight"
+    >
+      <ul
+        v-if="active"
+        class="absolute w-full mt-7 bg-light-50 border-dark-600"
+      >
+        <slot />
+      </ul>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiDropdown',
+  props: {},
+  data() {
+    return {
+      active: false,
+    }
+  },
+}
+</script>
---
+++ components/UiDropdownItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li
+    class="px-2 py-2 border-l-2 border-r-2 border-b-2 border-dark-600 transition duration-150 hover:(bg-teal-500 bg-opacity-10 cursor-pointer)"
+  >
+    <slot />
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'UiDropdownItem',
+}
+</script>
