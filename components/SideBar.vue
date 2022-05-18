df083a99f94375b80cf6f7863990aa58110cbe53
KreskoLab <redeezol@gmail.com>
2022-05-18 16:33:28 +0300

    rework (SideBar): update computed

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,8 +1,8 @@
 <template>
   <aside
-    class="hidden lg:(block fixed z-50 h-[calc(100%-7.5rem)] top-30 bg-amber-50 py-6 w-[320px] flex-none pl-6 overflow-y-auto overflow-x-hidden border-r-2 border-dark-600)"
+    class="hidden lg:(block fixed z-50 h-[calc(100%-7.5rem)] top-30 py-6 w-[320px] flex-none pl-6 overflow-y-auto overflow-x-hidden border-r-2 border-dark-600)"
   >
-    <div :class="subcategories_class">
+    <div :class="{ 'border-b-1 border-gray-500 w-11/12': categoryPage }">
       <SideBarSubcategoriesList />
     </div>
 
@@ -19,13 +19,13 @@
 export default {
   name: 'SideBar',
   computed: {
-    subcategories_class() {
+    categoryPage() {
       if (
         this.$route.name === 'category-id-subcategory' ||
         this.$route.name === 'category-id-subcategory-product-page'
       ) {
-        return 'border-b-1 border-gray-500 w-11/12'
-      } else return ''
+        return true
+      } else return false
     },
   },
 }
